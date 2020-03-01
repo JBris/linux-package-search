62e900e222d7a9dd7233b1a38c49df893a4e9f8b
JBris <james.a.bristow.contact@gmail.com>
2020-03-01 15:57:33 +1300

    Adding node-cache

--- server/src/routes/linux.js
+++ server/src/routes/linux.js
@@ -4,7 +4,10 @@ const apiSecret = require("../middleware/api-secret");
 const controller = require('../controllers/linux');
 
 router.get('/search/:distribution/:package', apiSecret.authenticate, controller.search);  
+router.get('/search-raw/:distribution/:package', apiSecret.authenticate, controller.searchRaw);  
+
 router.get('/view/:distribution/:package', apiSecret.authenticate, controller.view);  
+router.get('/view-raw/:distribution/:package', apiSecret.authenticate, controller.viewRaw);  
 
 module.exports = router;
  
