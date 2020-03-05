89330a51c8c6e8a9d9fc7386d11bf8551198dde9
JBris <james.a.bristow.contact@gmail.com>
2020-03-05 20:08:13 +1300

    Adding swagger docs for archive routes

--- server/src/routes/linux.js
+++ server/src/routes/linux.js
@@ -12,6 +12,7 @@ const controller = require('../controllers/linux');
  *     name: distribution
  *     schema:
  *       type: string
+ *       enum: [ debian, ubuntu, fedora ] 
  *     required: true
  *     description: The Linux distribution.
  * 
@@ -58,6 +59,30 @@ const controller = require('../controllers/linux');
  *     type: array
  *     items:
  *       $ref: '#/definitions/PackageInformation'
+ * 
+ *   DeletionConfirmation:
+ *     type: object
+ *     properties:
+ *       status:
+ *          type: string
+ *          description: The status of the deleted archived information.
+ *       quantity:
+ *          type: integer
+ *          description: The number of deleted rows.
+ *     description: A confirmation of the deletion request.   
+ * 
+ *   SearchQuery:
+ *     type: object
+ *     properties:
+ *       search_query:
+ *          type: string
+ *          description: The search query value associated with the package information.
+ *     description: A search query object attached to the package information.   
+ * 
+ *   SearchQueryCollection:
+ *     type: array
+ *     items:
+ *       $ref: '#/definitions/SearchQuery'
  */
 
 /**
@@ -184,9 +209,128 @@ router.get('/view/:distribution/:package', apiSecret.authenticate, controller.vi
  */
 router.get('/view-raw/:distribution/:package', apiSecret.authenticate, controller.viewRaw);  
 
+/**
+ * @swagger
+ * 
+ * /api/v1/linux/archive/search/{distribution}/{package}:
+ *   get:
+ *     tags:
+ *       - linux
+ *     description: Get a list of package names matching the search query for the specified distribution.
+ *     parameters:
+ *       - $ref: '#/parameters/distribution'
+ *       - $ref: '#/parameters/package'
+ *     security:
+ *       - ApiSecretAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: The archived package information results for the specified distribution.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/SearchQueryCollection'
+ *       400:
+ *         description: Return an error message if the archive search attempt fails.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Error'
+ */
 router.get('/archive/search/:distribution/:package', apiSecret.authenticate, controller.archiveSearch);  
+
+/**
+ * @swagger
+ * 
+ * /api/v1/linux/archive/view/{distribution}/{package}:
+ *   get:
+ *     tags:
+ *       - linux
+ *     description: Return an array of archived package information for the specified distribution.
+ *     parameters:
+ *       - $ref: '#/parameters/distribution'
+ *       - $ref: '#/parameters/package'
+ *     security:
+ *       - ApiSecretAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: The array of archived package information for the specified distribution.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/PackageInformationCollection'
+ *       400:
+ *         description: Return an error message if the archive view attempt fails.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Error'
+ */
 router.get('/archive/view/:distribution/:package', apiSecret.authenticate, controller.archiveView);  
-router.post('/archive/:distribution/:package', apiSecret.authenticate, controller.archiveSave);  
+
+/**
+ * @swagger
+ * 
+ * /api/v1/linux/archive/{distribution}/{package}:
+ *   post:
+ *     tags:
+ *       - linux
+ *     description: Save the specified package to the archive for the specified distribution.
+ *     parameters:
+ *       - $ref: '#/parameters/distribution'
+ *       - $ref: '#/parameters/package'
+ *     security:
+ *       - ApiSecretAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: The archived package information results for the specified distribution.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/PackageInformationCollection'
+ *       400:
+ *         description: Return an error message if the archival attempt fails.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Error'
+ */
+router.post('/archive/:distribution/:package', apiSecret.authenticate, controller.archiveSave); 
+
+/**
+ * @swagger
+ * 
+ * /api/v1/linux/archive/{distribution}/{package}:
+ *   delete:
+ *     tags:
+ *       - linux
+ *     description: Delete the specified package from the archive for the specified distribution.
+ *     parameters:
+ *       - $ref: '#/parameters/distribution'
+ *       - $ref: '#/parameters/package'
+ *     security:
+ *       - ApiSecretAuth: []
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: The archive deletion results for the specified distribution.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/DeletionConfirmation'
+ *       400:
+ *         description: Return an error message if the deletion attempt fails.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/definitions/Error'
+ */
 router.delete('/archive/:distribution/:package', apiSecret.authenticate, controller.archiveDelete);  
 
 module.exports = router;
